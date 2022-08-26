199b7126b2b87917e226d1c1da62a87d681f5154
VladimirMel0 <vladimiraparecido3@gmail.com>
2022-08-26 17:07:30 -0300

    chamada de erro da validação bad request

--- back/api/src/main/java/br/com/login/api/Model/Usuario.java
+++ back/api/src/main/java/br/com/login/api/Model/Usuario.java
@@ -6,6 +6,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 
 @Entity
 @Table(name = "Usuario")
@@ -15,12 +19,22 @@ public class Usuario {
     @Column(name = "id")
     private int id;
 
+    @NotBlank (message = "O nome é obrigatório!")
+    @Size(min = 3, message = "O nome deve ter no minimo 3 caracteres!")
     @Column(name = "nome", length = 200, nullable = false)
     private String nome;
+    
+    @Email (message = "Insira um email válido!")
+    @NotBlank (message = "O email é obrigatório!")
     @Column(name = "email", length = 100, nullable = false)
     private String email;
+
+    @NotBlank (message = "A senha é obrigatório!")
     @Column(name = "senha", columnDefinition = "TEXT", nullable = false)
     private String senha;
+
+    @NotBlank (message = "O telefone é obrigatório!")
+    @Size(min = 11, max = 11, message = "Insira um telefone válido!")
     @Column(name = "telefone", length = 25, nullable = false)
     private String telefone;
 
